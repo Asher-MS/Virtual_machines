e0de293113fe6d440346ce11b7a8e5a0013d3c51
Asher Mathews <msasher123@gmail.com>
2021-06-06 20:53:09 +0530

    added the main file

---
+++ README.md
@@ -0,0 +1,2 @@
+<h3>Setup Scripts for Debian Based VMs</h3>
+A script for setting up development environment on Debian based OSs to avoid the hassle each time i create a new VM
---
+++ setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# assumes Bash to be the prefered shell
+
+sudo apt update
+sudo apt install terminator
+
+echo "INSTLLATION STARTED"
+sudo apt update
+sudo apt install gcc
+sudo apt install g++
+sudo apt update
+sudo apt install docker
+sudo apt install docker.io
+sudo apt update
+
+
+#install VScode
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code
+
+
+#install Sublime
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo add-apt-repository "deb https://download.sublimetext.com/ apt/stable/"
+sudo apt update
+sudo apt install sublime-text
+
+
+echo "INSTLLATION FINISHED"
